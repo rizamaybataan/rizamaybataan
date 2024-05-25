e75315badd23b30f8813190a3c7c7c7898949fd1
rizamaybataan <bataanrhiza@gmail.com>
2024-05-25 18:13:12 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const btnLike1 = document.getElementById("btnlike1")
+const countLike1 = document.getElementById("countLiked")
+
+function clickLiked(){
+     let totalLikes = parseInt (countLike1.value + 1
+     countLike1.textContent = totallikes.toString()
+}
