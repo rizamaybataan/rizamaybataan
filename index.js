2aed3379f15a24b55d7db2a1d8b11493d46a3a4c
rizamaybataan <bataanrhiza@gmail.com>
2024-06-08 18:18:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const commentbox = document.getElementById("commentbox")
 function submitComment(){
 
 commentbox.textContent += comment.value.toString() + "\n"
-
+comment.value=""
 }
 
 submit.addEventListener("click", submitComment)
